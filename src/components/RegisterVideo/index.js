9302629776fc89bf798479ccd9c5f57e21ed05aa
Bruno Barqueta Salaar <bruno.salaar@ncc.org.br>
2022-12-27 10:32:48 -0300

    connection with supabase

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,5 +1,6 @@
 import { StyledRegisterVideo } from "./styles"
 import React from 'react'
+import { createClient } from "@supabase/supabase-js"
 
 const useForm = (props) => {
     const [values, setValues] = React.useState(props.initialValues);
@@ -18,6 +19,10 @@ const useForm = (props) => {
         }
     };
 }
+
+const PROJECT_URL = "https://ptqfpodvdwrhmrgamhsu.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cWZwb2R2ZHdyaG1yZ2FtaHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIwMTYwNDgsImV4cCI6MTk4NzU5MjA0OH0.1mfR6zLo8CYp8pFsDt1AvOpL9PA8p2MfWAzfhoC-Vb4";
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 const RegisterVideo = () => {
     const formCadastro = useForm({
